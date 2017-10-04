4a133385cb77dcfdf4dbbcf07f792b22de8797db
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-10-04 20:27:17 +0530

    Create Question2.cpp

---
+++ Question2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+void Number( int A)
+{	
+	static int x = 1;
+	if(A>0)
+	{	
+		cout << x << endl;
+		x++;
+		Number(A-1);
+	}		
+}
+int main()
+{
+	int a,b;
+	cout << "Enter the number:	";
+	cin >> a;
+	if( a>0)
+		Number(a);
+	else
+		cout << "Enter a natural number.";
+	return 0;
+}
